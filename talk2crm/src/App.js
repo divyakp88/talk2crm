3281a7ec6dc1fcdb04169aff4cfd699937733738
divyakp88 <kpdivya08@gmail.com>
2026-01-27 17:02:01 +0530

    api end point calling in front end changed

--- talk2crm/src/App.js
+++ talk2crm/src/App.js
@@ -42,7 +42,7 @@ function App() {
       formData.append("file", audioBlob.blob, "recording.wav");
 
       const response = await axios.post(
-        "https://talk2crm.onrender.com",
+        "https://talk2crm.onrender.com/transcribe",
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
       );
